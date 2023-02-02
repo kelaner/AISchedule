f403f548e56e59ee4acadc4721c3e06bcb95763c
laner <lanercifang@outlook.com>
2023-02-02 23:57:48 +0800

    build:tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,6 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['src/**/*.{js,jsx}'],
-    theme: {
-        extend: {},
-    },
-    plugins: [require('@tailwindcss/aspect-ratio'), require('@tailwindcss/line-clamp'), require('@tailwindcss/typography'), require('@tailwindcss/forms')],
-}
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
+};
